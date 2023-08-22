5fa659237cddc9e2802ab2cf873c604a5099b96f
Harsh Dixit <113188259+HarshDixit1@users.noreply.github.com>
2023-08-22 17:30:36 +0000

    container config

---
+++ 01_basic/Variable.js
@@ -0,0 +1,17 @@
+const Name ="harsh dixit"
+let age="19"
+var caste="hindu"
+let gmailId="harsh@gmail.com"
+const gmailPassword="21345@135"
+var heigth;
+
+
+// Name="vinay"    not possible as it declare as constant
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([Name,age,caste,gmailId,gmailPassword,heigth ])
+// console.log(Name);
