de4151610bc1fac08311139f3a1207aff1509ab0
Harsh Dixit <113188259+HarshDixit1@users.noreply.github.com>
2023-08-23 15:37:09 +0000

    datatypes js

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,40 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) // here it give error b/c we are using node js,not browser
+
+//DATA TYPES
+
+let name="harsh" // string
+let age=19 //number
+var pass =true //boolean
+
+//string
+//number
+//boolean
+//bigint
+//null=>standalone value
+//undefined
+//symbol=> unique
+
+//object
+
+console.log(typeof null);
+
+// typeof null=>Object
+// typeof undefined=> undefined
+
+/*follow the link to learn more
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+*/
+
+
+/*
+Type	typeof return value	Object wrapper
+Null	    "object"	         N/A
+Undefined	"undefined"	         N/A
+Boolean	    "boolean"	         Boolean
+Number	    "number"	         Number
+BigInt	    "bigint"	         BigInt
+String	    "string"	         String
+Symbol	    "symbol"	         Symbol
+*/
