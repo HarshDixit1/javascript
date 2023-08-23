a67411dd84ba9163ca0269af01ab3299c98bc1a0
Harsh Dixit <113188259+HarshDixit1@users.noreply.github.com>
2023-08-23 16:11:11 +0000

    conversion of datatype

---
+++ 01_basic/datatype_Conversion.js
@@ -0,0 +1,32 @@
+// let score=33
+
+// console.log(typeof score);
+// let valueInt =Number(score)
+// console.log(typeof valueInt);
+// console.log(valueInt);
+
+/* NOTE ->
+"33"->33 (easily convert)
+"33abc"->NaN (Not a Number)
+undefined->NaN
+null-> zero (0)
+true->1
+false->0
+*/
+
+
+// let age =12
+// let Instring=String(age)
+// console.log(Instring);// output is 12
+// console.log(typeof Instring); // it give string type
+
+
+/*
+let x=undefined
+let y=null(x)
+console.log(typeof y)
+console.log(y); 
+*/
+
+// Note : During conversion of null to undefined and undefined to null ,
+// it says null is not a function and undefined not a function. it give error
