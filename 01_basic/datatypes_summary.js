9df81c2911c7c4856d85a5c1190a692450534c98
Harsh Dixit <113188259+HarshDixit1@users.noreply.github.com>
2023-08-24 06:06:47 +0000

    summary of datatypes

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : string, Number, boolean, null, undefined, symbol, bigInt
+
+
+// Non Primitive (Reference type)
+
+// object, Array,function
+
+/*Note:JavaScript is a dynamically typed language, which means that data types 
+of variables are determined by the value they hold at runtime and can change 
+throughout the program as we assign different values to them.*/
+
+
+// let Id1=Symbol("harsh")
+// let Id2=Symbol("harsh")  
+// console.log(Id1===Id2); // as we can see output is false b/c symbol used to give unique identity
+
+
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object => object
+       */   
