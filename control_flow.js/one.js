acbc0ea94f4002bc7c494077ccea4924f80222ce
Harsh Dixit <113188259+HarshDixit1@users.noreply.github.com>
2023-09-15 16:40:28 +0000

    IF ELSE etc

---
+++ control_flow.js/one.js
@@ -0,0 +1,14 @@
+// <,>,<=,>=,==,!=,===(strictly equal),!==
+
+
+// const score=200
+// if(score>100)
+// {
+//     const user="fly"
+//     console.log(`user power: ${user}`);
+// }  
+// console.log(`user power: ${user}`); // give error- user is not defined
+
+const friend="dishant"
+// if(friend=="dishant") console.log("he is my best buddy");
+console.log("he");
